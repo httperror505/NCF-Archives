75344bf0e6dd68f15a8d8d4fc2e0c9bc08ce17c6
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2024-01-02 19:10:22 +0800

    Fuzzywuzzy to Fuzzball library

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fuzzy = require('fuzzywuzzy');
+const fuzzball = require('fuzzball');
 
 const app = express();
-// const port = 3000;
+const port = process.env.PORT || 3000;
 
-const documents = ['hello, world', 'my world' ,'hello mom' ,'the usage of it' ,'if and else' ,'uses of it' ,'hello and else' ,'out of the world' ,'yes mom' ,'yes of it' ,'if and mom'];
+const documents = [
+  'hello, world', 'my world' ,'hello mom' ,'the usage of it' ,
+  'if and else' ,'uses of it' ,'hello and else' ,'out of the world' ,
+  'yes mom' ,'yes of it' ,'if and mom'
+];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,13 +17,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/search', (req, res) => {
   const query = req.query.query;
 
-  // Use fuzzywuzzy to perform a fuzzy search on documents
-  const results = fuzzy.filter(query, documents).map(result => result.string);
+  // Use fuzzball to perform a fuzzy search on documents
+  const options = {
+    force_ascii: false,  // Allows non-ASCII characters in strings
+  };
+
+  const results = fuzzball.extract(query, documents, options).map(result => result[0]);
 
   res.json(results);
 });
 
-// app.listen(port, () => {
-//   console.log(`Server listening at http://localhost:${port}`);
-// });
-app.listen(process.env.PORT || 3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
